efde224b4106a3199fb904d95bc02dc67beff5be
Carlos Figueiras Valverde <cfigueirasvalverde@danielcastelao.org>
2019-11-14 12:41:59 +0100

    boletin9_2

---
+++ Boletin9_2/src/boletin9_2/Boletin9_2.java
@@ -0,0 +1,16 @@
+package boletin9_2;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin9_2 {
+
+
+    public static void main(String[] args) {
+       SumaProducto tomate = new SumaProducto();
+        System.out.println("Suma: " + tomate.suma() + "\nProducto: " 
+                + tomate.producto());
+    }
+        
+    }
