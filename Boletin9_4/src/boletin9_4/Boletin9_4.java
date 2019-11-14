1ec42be52e8ccc01b8541141b7df96a8333e0cba
Carlos Figueiras Valverde <cfigueirasvalverde@danielcastelao.org>
2019-11-14 12:48:16 +0100

    boletin9_4

---
+++ Boletin9_4/src/boletin9_4/Boletin9_4.java
@@ -0,0 +1,16 @@
+package boletin9_4;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin9_4 {
+
+
+    public static void main(String[] args) {
+         Tabla tomate = new Tabla();
+        tomate.tablaMultiplicar();
+    }
+        
+        
+}
