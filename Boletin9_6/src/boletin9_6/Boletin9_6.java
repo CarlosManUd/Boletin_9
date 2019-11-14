32ae06f229a0ba62242eefdc092d559aa29dca79
Carlos Figueiras Valverde <cfigueirasvalverde@danielcastelao.org>
2019-11-14 12:56:58 +0100

    boletin9_6

---
+++ Boletin9_6/src/boletin9_6/Boletin9_6.java
@@ -0,0 +1,15 @@
+package boletin9_6;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin9_6 {
+
+    
+    public static void main(String[] args) {
+        Sueldos tomate = new Sueldos();
+        tomate.lecturaSueldos();
+    }
+    
+}
