1614fdb7cde0329d467c2c9d9eba557a3e7b1956
Carlos Figueiras Valverde <cfigueirasvalverde@danielcastelao.org>
2019-11-14 12:56:50 +0100

    boletin9_5

---
+++ Boletin9_5/src/boletin9_5/Boletin9_5.java
@@ -0,0 +1,15 @@
+package boletin9_5;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin9_5 {
+
+   
+    public static void main(String[] args) {
+         Serie tomate = new Serie();
+        tomate.serieSuma();
+    }
+    
+}
