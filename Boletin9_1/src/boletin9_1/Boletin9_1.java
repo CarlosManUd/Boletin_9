7a24fa10be2e1677e938b725d6debcff16aaa940
Carlos Figueiras Valverde <cfigueirasvalverde@danielcastelao.org>
2019-11-14 12:40:00 +0100

    boletin9_1

---
+++ Boletin9_1/src/boletin9_1/Boletin9_1.java
@@ -0,0 +1,16 @@
+package boletin9_1;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin9_1 {
+
+
+    public static void main(String[] args) {
+        Contador tomate = new Contador();
+        tomate.grupoNumeros();
+    }
+        
+        
+    }
